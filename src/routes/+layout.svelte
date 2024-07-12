22c477eb139b9bd62f8c4dcda7d45fa3dac15b7d
Justin Edmund <383021+jedmund@users.noreply.github.com>
2024-07-12 08:44:51 -0400

    Update home page and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-	import '$lib/styles/reset.css'
-	import '$lib/styles/fonts.css'
-	import '$lib/styles/globals.scss'
+	import Avatar from '$components/Avatar.svelte'
+	import Squiggly from '$components/Squiggly.svelte'
 </script>
 
 <svelte:head>
@@ -10,10 +9,18 @@
 		name="description"
 		content="Justin Edmund is a software designer based in San Francisco, California."
 	/>
+	<link href="/assets/styles/reset.css" rel="stylesheet" />
 </svelte:head>
 
 <main>
 	<section class="page">
+		<header>
+			<h1 aria-label="@jedmund">
+				<Avatar />
+			</h1>
+			<Squiggly text="@jedmund is a software designer" />
+		</header>
+
 		<slot />
 	</section>
 </main>
@@ -29,8 +36,28 @@
 		background: var(--page-color);
 		border-radius: 16px;
 		box-sizing: border-box;
-		margin: 48px auto 48px;
-		padding: 40px;
-		width: 784px;
+		display: flex;
+		flex-direction: column;
+		gap: $unit-4x;
+		margin: $unit-6x auto $unit-6x;
+		padding: $unit-5x;
+		width: 100%;
+		max-width: 784px;
+
+		@include breakpoint('phone') {
+			margin-top: $unit-2x;
+			margin-bottom: $unit-3x;
+			padding: $unit-3x;
+		}
+
+		@include breakpoint('small-phone') {
+			padding: $unit-2x;
+		}
+
+		header {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
 	}
 </style>
