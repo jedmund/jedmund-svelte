a6780aa8594f7f0ff53c69cdfe011506c720078d
Justin Edmund <383021+jedmund@users.noreply.github.com>
2024-07-12 08:42:45 -0400

    Update config
    * Add autoprefexer
    * Properly set up SVG components
    * Properly set up SCSS preprocessing

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,69 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
+import svg from '@poppanator/sveltekit-svg'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [
+		sveltekit(),
+		svg({
+			includePaths: ['./src/assets/icons/'],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: 'preset-default',
+						params: {
+							overrides: {
+								removeViewBox: false
+							}
+						}
+					},
+					{
+						name: 'removeAttrs',
+						params: {
+							attrs: ['fill', 'stroke']
+						}
+					},
+					{
+						name: 'addAttributesToSVGElement',
+						params: {
+							attributes: [{ fill: 'currentColor' }]
+						}
+					}
+				]
+			}
+		}),
+		svg({
+			includePaths: ['./src/assets/illos/'],
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: 'preset-default',
+						params: {
+							overrides: {
+								removeViewBox: false
+							}
+						}
+					}
+				]
+			}
+		})
+	],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+          @import './src/assets/styles/variables.scss';
+          @import './src/assets/styles/fonts.scss';
+          @import './src/assets/styles/themes.scss';
+          @import './src/assets/styles/globals.scss';
+        `
+			}
+		},
+		postcss: {
+			plugins: [autoprefixer]
+		}
+	}
+})
