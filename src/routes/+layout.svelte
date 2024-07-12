a0cd967f0a4fab5dbbabb7a19fc1e59a2b74725c
Justin Edmund <383021+jedmund@users.noreply.github.com>
2024-07-12 03:37:29 -0400

    Basic styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import '$lib/styles/reset.css'
+	import '$lib/styles/fonts.css'
+	import '$lib/styles/globals.scss'
+</script>
+
+<svelte:head>
+	<title>@jedmund</title>
+	<meta
+		name="description"
+		content="Justin Edmund is a software designer based in San Francisco, California."
+	/>
+</svelte:head>
+
+<main>
+	<section class="page">
+		<slot />
+	</section>
+</main>
+
+<style lang="scss">
+	:global(html) {
+		background: var(--bg-color);
+		color: var(--text-color);
+		font-family: 'cstd', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	}
+
+	.page {
+		background: var(--page-color);
+		border-radius: 16px;
+		box-sizing: border-box;
+		margin: 48px auto 48px;
+		padding: 40px;
+		width: 784px;
+	}
+</style>
