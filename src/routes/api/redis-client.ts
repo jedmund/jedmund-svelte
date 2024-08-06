af3a72dc8b904d4f90f50ac8def89d38dc83616a
Justin Edmund <justin@jedmund.com>
2024-08-05 23:45:08 -0700

    Update Redis client config

--- src/routes/api/redis-client.ts
+++ src/routes/api/redis-client.ts
@@ -1,8 +1,8 @@
 import { Redis } from 'ioredis'
 
 const redis = new Redis({
-	host: process.env.REDIS_HOST || 'localhost',
-	port: parseInt(process.env.REDIS_PORT || '6379')
+	host: process.env.REDISHOST || 'localhost',
+	port: parseInt(process.env.REDISPORT || '6379')
 })
 
 export default redis
