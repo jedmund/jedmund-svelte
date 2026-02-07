a8ea706b91d72c69af9468aaa8cffb7ecfc14f64
Justin Edmund <justin@jedmund.com>
2026-02-06 21:07:42 -0800

    add custom extensions (smiliereplacer, fontsize) and fix editor imports

--- src/lib/components/edra/editor.ts
+++ src/lib/components/edra/editor.ts
@@ -1,19 +1,24 @@
 import { type Content, Editor, type EditorOptions, type Extensions } from '@tiptap/core';
+import CharacterCount from '@tiptap/extension-character-count';
+import Color from '@tiptap/extension-color';
 import Highlight from '@tiptap/extension-highlight';
-import { TaskItem, TaskList } from '@tiptap/extension-list';
+import Placeholder from '@tiptap/extension-placeholder';
 import Subscript from '@tiptap/extension-subscript';
 import Superscript from '@tiptap/extension-superscript';
+import TaskItem from '@tiptap/extension-task-item';
+import TaskList from '@tiptap/extension-task-list';
 import TextAlign from '@tiptap/extension-text-align';
-import { Color, FontSize, TextStyle } from '@tiptap/extension-text-style';
+import TextStyle from '@tiptap/extension-text-style';
 import Typography from '@tiptap/extension-typography';
-import { CharacterCount, Placeholder } from '@tiptap/extensions';
 import StarterKit from '@tiptap/starter-kit';
 import AutoJoiner from 'tiptap-extension-auto-joiner';
 import { ColorHighlighter } from './extensions/ColorHighlighter.js';
 import SearchAndReplace from './extensions/FindAndReplace.js';
+import { SmilieReplacer } from './extensions/SmilieReplacer.js';
+import { FontSize } from './extensions/FontSize.js';
 import { Table, TableCell, TableHeader, TableRow } from './extensions/table/index.js';
 import 'katex/dist/katex.min.css';
-import { Markdown } from '@tiptap/markdown';
+import Markdown from 'tiptap-markdown';
 import { InlineMathReplacer } from './extensions/InlineMathReplacer.js';
 import strings from './strings.js';
 
@@ -85,6 +90,7 @@ export default (
 				nested: true
 			}),
 			SearchAndReplace,
+			SmilieReplacer,
 			AutoJoiner,
 			Table,
 			TableHeader,
